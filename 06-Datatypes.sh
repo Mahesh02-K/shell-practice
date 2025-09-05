6fe0b254c07cef6d47792245e5b2815f378402b3
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-05 19:43:28 +0530

    variables example

--- 06-Datatypes.sh
+++ 06-Datatypes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-$Number1=100
-$Number2=200
+$N1=100
+$N2=200
 
 Timestamp=$(date)
 
 echo "Script executed at :: $Timestamp"
-Sum=$(($Number1+$Number2))
-echo "sum of Number1 and Number2 is :: $Sum"
+Sum=$(($N1+$N2))
+echo "sum of $N1 and $N2 is :: $Sum"
