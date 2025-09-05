15978b47ca56e5595545c42321630edb0f9bcbac
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-05 19:46:03 +0530

    variables example

--- 06-Datatypes.sh
+++ 06-Datatypes.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# $N1=100
-# $N2=200
+N1=100
+N2=200
 
-# Timestamp=$(date)
+Timestamp=$(date)
 
-# echo "Script executed at :: $Timestamp"
-# Sum=$(($N1+$N2))
-# echo "sum of $N1 and $N2 is :: $Sum"
+echo "Script executed at :: $Timestamp"
+Sum=$(($N1+$N2))
+echo "sum of $N1 and $N2 is :: $Sum"
 
 # this is comment
-NUMBER1=100
-NUMBER2=200
+# NUMBER1=100
+# NUMBER2=200
 
-TIMESTAMP=$(date)
-echo "Script executed at: $TIMESTAMP"
-SUM=$(($NUMBER1+$NUMBER2))
+# TIMESTAMP=$(date)
+# echo "Script executed at: $TIMESTAMP"
+# SUM=$(($NUMBER1+$NUMBER2))
 
-echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+# echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
