b7fa4fa67139afd10cd59384f7298ccafd8e9efe
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-05 19:28:23 +0530

    variables example

---
+++ 06-Datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$Number1=100
+$Number2=200
+
+Timestamp=$(date)
+
+echo "Script executed at :: $Timestamp"
+sum=$(Number1+Number2)
+echo "sum of Number1 and Number2 is :: $Sum"
---
+++ 07-Arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("SALAAR" "KALKI2898AD" "SAAHO" "RAJASAAB")
+
+echo "${MOVIES[0]}"
+echo "${MOVIES[3]}"
+echo "${MOVIES[5]}"
+
+echo "All movies :: ${MOVIES[@]}"
