5609de98e3a38751d120a6edd768d2df34aa9d3c
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-05 18:03:40 +0530

    variables pass through arguments

---
+++ 04-Variables-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1 :: Hi $Person2! How are you?"
+echo "$Person2 :: Hi $Person1! I'm good, How about you?"
+echo "$Person1 :: I'm fine! What's going on?"
+echo "$Person2 :: Today we proposed new GST reforms and will activate them from september 22nd"
+echo "$Person1 :: Great to hear this wonderful news from you $Person2"
+echo "$Person2 :: Thank you $Person1"
