45d72167697f2a829da28a5324233e9a375475ff
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-05 17:54:12 +0530

    variables

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -4,7 +4,7 @@ Person1=Trump
 Person2=Musk
 
 echo "$Person1 : Hi $Person2! How are you?"
-echo "$Person2 : I'm good! what's up?"
+echo "$Person2 : Hi $Person1, I'm good! what's up?"
 echo "$Person1 : What's your plan on this weekend?"
 echo "$Person2 : Scheduled to visit India for proposal to establish Tesla manufacturing plant"
-echo "$Person1 : Ok! Good luck."
+echo "$Person1 : Ok! Good luck $Person2."
