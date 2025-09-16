d67248a6b05af726ebc593fa08fc05798dbf3005
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-16 15:30:48 +0530

    setting error

--- 20-set.sh
+++ 20-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e #setting error, it is used to exit if we get any error in script
+# set -e #setting error, it is used to exit if we get any error in script
 
 echo "Hello Everyone"
 echo "How are you"
