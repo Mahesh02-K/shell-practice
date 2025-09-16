e98a9ce1c14902518aa50c1c540f2136555c6274
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-16 14:59:14 +0530

    Calling a script from another script

--- 17-script-1.sh
+++ 17-script-1.sh
@@ -4,5 +4,7 @@ GREETING="Hello Goodmorning!"
 echo "$GREETING"
 echo "PID of script-1 :: $$"
 
-./18-script-2.sh
+# ./18-script-2.sh -we need execute access if we want to use this.
+
+source ./18-script-2.sh
 
